dad56f1dfa96087a6f732fa616966c969b677bb9
Daniel Gächter <daniel.gaechter@uzh.ch>
2018-04-04 01:29:30 +0200

    Issue #1 Add buttons and button_area in main menu

---
+++ src/app/button-database.ts
@@ -0,0 +1,7 @@
+import { Button } from './Button';
+
+export const BUTTONS: Button[] = [
+  { id: 'menubutton-hostgame', name: 'Host Game', action: 'www.google.ch', image: '../assets/Gui_Elemente/GUI_MainMenu/Menubuttons.png' },
+  { id: 'menubutton-joingame', name: 'Join Game', action: 'www.google.ch', image: '../assets/Gui_Elemente/GUI_MainMenu/Menubuttons.png' },
+  { id: 'menubutton-manual', name: 'Manual', action: 'www.google.ch', image: '../assets/Gui_Elemente/GUI_MainMenu/Menubuttons.png' }
+];
--- src/app/main-menu/buttons/buttons.component.ts
+++ src/app/main-menu/buttons/buttons.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Button } from '../../button';
+import { BUTTONS } from '../../button-database';
 
 @Component({
   selector: 'app-buttons',
@@ -6,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buttons.component.css']
 })
 export class ButtonsComponent implements OnInit {
+  buttons = BUTTONS;
+  selectedButton: Button;
 
+  onSelect(button: Button): void {
+    this.selectedButton = button;
+  }
   constructor() { }
 
   ngOnInit() {
