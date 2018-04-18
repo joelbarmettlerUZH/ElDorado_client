c0528dd552d9bb7c395c30d690200ebaf398faea
mhoegger <32105585+mhoegger@users.noreply.github.com>
2018-04-18 18:34:06 +0200

    creating hexspaces but they are not displayed #9

--- src/app/in-game-screen/map/board/board.component.ts
+++ src/app/in-game-screen/map/board/board.component.ts
@@ -18,6 +18,8 @@ export class BoardComponent implements OnInit {
   public colors: string[];
   private xDim: number;
   private yDim: number;
+  private xWidth: number;
+  private xOffset: number;
 
   // xDim: Observable<number>;
 
@@ -30,12 +32,14 @@ export class BoardComponent implements OnInit {
     const x = 17;
     const y = 10;
     this.colors = ['basecamp', 'grass', 'mountain', 'river', 'rubble', 'sand'];
-    let xWidth = (100 / x);
-    xWidth = ((100 - xWidth / 2) / x);
-    const xOffset = (xWidth / 2);
-    this.hexspace = generateBoard(x, y);
-    createCSSSelector('.xOffset', 'margin-left: ' + xOffset.toString() + '%;');
-    choseOffsets(x);
+    this.xWidth = (100 / x);
+    this.xWidth = ((100 - this.xWidth / 2) / x);
+    this.xOffset = (this.xWidth / 2);
+    console.log(this.colors[2]);
+    this.hexspace = this.generateBoard(x, y);
+    console.log('hexspace length: ' + this.hexspace.length)
+    this.createCSSSelector('.xOffset', 'margin-left: ' + this.xOffset.toString() + '%;');
+    this.choseOffsets(x);
     console.log('pre tutti complexo stuffo');
     // let hexagons = this.boardService.getHexagons();
     // this.xDim = this.boardService.getBoard();
@@ -45,6 +49,7 @@ export class BoardComponent implements OnInit {
     // this.boardService.getBoard().subscribe(json => console.log(json.xdim));
     // this.boardService.getBoard().subscribe(val => this.xDim = val.xdim.valueOf());
     // this.boardService.getBoard().subscribe(val => this.yDim = val.ydim.valueOf());
+
     setTimeout(() => {
       console.log('Got and assigned x Dimension:', this.xDim);
     }, 2000);
@@ -52,103 +57,113 @@ export class BoardComponent implements OnInit {
       console.log('Got and assigned y Dimension:', this.yDim);
     }, 2000);
   }
-}
+
 
 // this.boardService.getBoard().subscribe(data => this.board = {xdim: data['xDim']});
 // generateBoard(x, y);
 // createCSSSelector(".xOffset", "margin-left: "+xOffset.toString()+"%;")
 // choseOffsets(x);
-function choseOffsets(x: number) {
-  console.log('Chosing offset now: ');
-  let hexSpaces: NodeListOf<Element> = document.getElementsByClassName('hex');
-  for (let i = 0; i < hexSpaces.length; i++) {
-    console.log(this.xWidth.toString() + '%');
-    let current: HTMLElement = hexSpaces[i] as HTMLElement;
-    current.style.width = this.xWidth.toString() + '%';
-    console.log(i);
-    if ((i % ((x * 2))) - x == 0) {
-      console.log('found one ' + i);
-      hexSpaces[i].className += ' xOffset';
+
+
+  choseOffsets(x: number) {
+    console.log('Chosing offset now: ');
+    let hexSpaces: NodeListOf<Element> = document.getElementsByClassName('hex');
+    for (let i = 0; i < hexSpaces.length; i++) {
+      console.log(this.xWidth.toString() + '%');
+      let current: HTMLElement = hexSpaces[i] as HTMLElement;
+      current.style.width = this.xWidth.toString() + '%';
+      console.log(i);
+      if ((i % ((x * 2))) - x == 0) {
+        console.log('found one ' + i);
+        hexSpaces[i].className += ' xOffset';
+      }
     }
   }
-}
 
-function generateBoard(x, y): string[] {
-  console.log('Generating board');
-  let arr: Array<string>;
-  for (let i = 0; i < x * y; i++) {
-    console.log('Looping ' + i);
-    let color: string = this.colors[Math.floor(Math.random() * this.colors.length)];
-    arr.push(color);
+
+
+  generateBoard(x, y): string[] {
+    console.log('Generating board');
+    let arr: string[] = new Array();
+    for (let i = 0; i < x * y; i++) {
+      console.log('Looping ' + i);
+      let color: string;
+      console.log('random number' + Math.floor(Math.random() * this.colors.length));
+      console.log('Looping ' + i);
+      color = this.colors[Math.floor(Math.random() * this.colors.length)];
+      arr.push(color);
+    }
+    console.log('Returning board: ' + arr.length);
+    return arr;
   }
-  console.log('Returning board');
-  return arr;
-}
 
-function createCSSSelector(selector, style) {
-  console.log('Generate CSS');
-  if (!document.styleSheets) return;
-  if (document.getElementsByTagName('head').length == 0) return;
 
-  let styleSheet, mediaType;
+  createCSSSelector(selector, style) {
+    console.log('Generate CSS');
+    if (!document.styleSheets) return;
+    if (document.getElementsByTagName('head').length == 0) return;
 
-  if (document.styleSheets.length > 0) {
-    for (let i = 0, l = document.styleSheets.length; i < l; i++) {
-      if (document.styleSheets[i].disabled) {continue; }
-      let media: MediaList = document.styleSheets[i].media;
-      mediaType = typeof media;
+    let styleSheet, mediaType;
 
-      if (mediaType === 'string') {
-        if (media.length == 0) { // || (media.('screen') !== -1)) {
-          styleSheet = document.styleSheets[i];
+    if (document.styleSheets.length > 0) {
+      for (let i = 0, l = document.styleSheets.length; i < l; i++) {
+        if (document.styleSheets[i].disabled) {
+          continue;
+        }
+        let media: MediaList = document.styleSheets[i].media;
+        mediaType = typeof media;
+
+        if (mediaType === 'string') {
+          if (media.length == 0) { // || (media.('screen') !== -1)) {
+            styleSheet = document.styleSheets[i];
+          }
+        }
+        else if (mediaType == 'object') {
+          if (media.mediaText === '' || (media.mediaText.indexOf('screen') !== -1)) {
+            styleSheet = document.styleSheets[i];
+          }
+        }
+
+        if (typeof styleSheet !== 'undefined')
+          break;
+      }
+    }
+
+    if (typeof styleSheet === 'undefined') {
+      let styleSheetElement = document.createElement('style');
+      styleSheetElement.type = 'text/css';
+      document.getElementsByTagName('head')[0].appendChild(styleSheetElement);
+
+      for (let i = 0; i < document.styleSheets.length; i++) {
+        if (document.styleSheets[i].disabled) {
+          continue;
+        }
+        styleSheet = document.styleSheets[i];
+      }
+
+      mediaType = typeof styleSheet.media;
+    }
+
+    if (mediaType === 'string') {
+      let l = styleSheet.rules.length;
+      for (let i = 0; i < l; i++) {
+        if (styleSheet.rules[i].selectorText && styleSheet.rules[i].selectorText.toLowerCase() == selector.toLowerCase()) {
+          styleSheet.rules[i].style.cssText = style;
+          return;
         }
       }
-      else if (mediaType == 'object') {
-        if (media.mediaText === '' || (media.mediaText.indexOf('screen') !== -1)) {
-          styleSheet = document.styleSheets[i];
+      styleSheet.addRule(selector, style);
+    } else if (mediaType === 'object') {
+      let styleSheetLength = (styleSheet.cssRules) ? styleSheet.cssRules.length : 0;
+      for (let i = 0; i < styleSheetLength; i++) {
+        if (styleSheet.cssRules[i].selectorText && styleSheet.cssRules[i].selectorText.toLowerCase() == selector.toLowerCase()) {
+          styleSheet.cssRules[i].style.cssText = style;
+          return;
         }
       }
-
-      if (typeof styleSheet !== 'undefined')
-        break;
+      styleSheet.insertRule(selector + '{' + style + '}', styleSheetLength);
     }
   }
 
-  if (typeof styleSheet === 'undefined') {
-    let styleSheetElement = document.createElement('style');
-    styleSheetElement.type = 'text/css';
-    document.getElementsByTagName('head')[0].appendChild(styleSheetElement);
-
-    for (let i = 0; i < document.styleSheets.length; i++) {
-      if (document.styleSheets[i].disabled) {
-        continue;
-      }
-      styleSheet = document.styleSheets[i];
-    }
-
-    mediaType = typeof styleSheet.media;
-  }
-
-  if (mediaType === 'string') {
-    let l = styleSheet.rules.length;
-    for (let i = 0; i < l; i++) {
-      if (styleSheet.rules[i].selectorText && styleSheet.rules[i].selectorText.toLowerCase() == selector.toLowerCase()) {
-        styleSheet.rules[i].style.cssText = style;
-        return;
-      }
-    }
-    styleSheet.addRule(selector, style);
-  } else if (mediaType === 'object') {
-    let styleSheetLength = (styleSheet.cssRules) ? styleSheet.cssRules.length : 0;
-    for (let i = 0; i < styleSheetLength; i++) {
-      if (styleSheet.cssRules[i].selectorText && styleSheet.cssRules[i].selectorText.toLowerCase() == selector.toLowerCase()) {
-        styleSheet.cssRules[i].style.cssText = style;
-        return;
-      }
-    }
-    styleSheet.insertRule(selector + '{' + style + '}', styleSheetLength);
-  }
 }
-
-
 
