1fd2aa0d3c19d369943f363b6eca8d78c74c22be
joelbarmettler.UZH <joel.barmettler@uzh.ch>
2018-04-19 15:14:52 +0200

    Implemented Services for Room #12

--- src/app/shared/services/room.service.ts
+++ src/app/shared/services/room.service.ts
@@ -1,68 +1,36 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs/Observable';
-import {of} from 'rxjs/observable/of';
-import {Room} from '../models/Room';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {catchError, map, tap} from 'rxjs/operators';
+import {restUrl} from './RESTurl';
+import {Http} from '@angular/http';
+import {CreateRoom} from '../models/createRoom';
+import {User} from '../models/User';
 
 @Injectable()
 export class RoomService {
 
-  private elDoradoUrl = 'https://sopra-fs18-group17.herokuapp.com/api/v0/Room';  // URL to web api
-  constructor(private http: HttpClient) {
+  private baseUrl = restUrl.getBaseUrl();
+  private roomsUrl = "${baseUrl}/Room";
+  private roomUrl = "${baseUrl}/Room/${roomid}";
+  constructor(private http: Http) { }
+
+  // returns all rooms
+  public getAllRooms() {
+    return this.http.get(this.roomsUrl).map(res => res.json());
   }
 
-  getRooms(): Observable<any[]> {
-    return this.http.get<any[]>(this.elDoradoUrl)
-      .pipe(
-        catchError(this.handleError('getRooms', []))
-      );
+  // Returns a specific room with id roomId
+  public getRoom(roomId: number){
+    return this.http.get(this.roomUrl).map(res => res.json());
   }
 
-  //
-  // Other APIs may bury the data that you want within an object.
-  // You might have to dig that data out by processing the
-  // Observable result with the RxJS map operator.
-  // Although not discussed here, there's an example of map in this getHeroNo404() method:
-
-  // getHeroNo404<Data>(id: number): Observable<Hero> {
-  //   const url = `${this.heroesUrl}/?id=${id}`;
-  //   return this.http.get<Hero[]>(url)
-  //     .pipe(
-  //       map(heroes => heroes[0]), // returns a {0|1} element array
-  //       tap(h => {
-  //         const outcome = h ? `fetched` : `did not find`;
-  //         this.log(`${outcome} hero id=${id}`);
-  //       }),
-  //       catchError(this.handleError<Hero>(`getHero id=${id}`))
-  //     );
-  // }
-
-  /** GET room by id. Will 404 if id not found */
-  getRoom(id: number): Observable<Room> {
-    const url = `${this.elDoradoUrl}/${id}`;
-    return this.http.get<Room>(url).pipe(
-      catchError(this.handleError<Room>(`getRoom id=${id}`))
-    );
+  // Creates a new room with the createRoom properties, returns the newly created room
+  public createRoom(createRoom: CreateRoom){
+    return this.http.post(this.roomsUrl, createRoom).map(res => res.json());
   }
 
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      // this.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
+  //adds a user to a room and returns the modified room
+  public addUser(user: User, gameId: number){
+    return this.http.put(this.roomUrl, user).map(res => res.json());
   }
+
 }
