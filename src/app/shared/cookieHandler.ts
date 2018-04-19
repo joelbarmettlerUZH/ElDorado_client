9761dc888dc89c1bdc6cebcf3a531385a2858650
joelbarmettler.UZH <joel.barmettler@uzh.ch>
2018-04-19 15:35:40 +0200

    Added template for cookie handler to later implement local storage or cookie #120

---
+++ src/app/shared/cookieHandler.ts
@@ -0,0 +1,17 @@
+export class CookieHandler {
+
+  static saveCookie(playerId: number, token: string): void{
+    // TODO: SAVE playerId and token to cookie or local storage
+  }
+
+  static readId(): number{
+    // TODO: Read id out of cookie or local storage
+    return 1;
+  }
+
+  static readToken(): string{
+    // TODO: Read token out of cookie or local storage
+    return "TESTTOKEN";
+  }
+
+}
