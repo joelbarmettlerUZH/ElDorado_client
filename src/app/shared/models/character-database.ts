2077bfa0c7d7ea58f043db0a897efca0f0f4e4a5
daniela flueeli <daniela.flueeli@uzh.ch>
2018-04-21 19:56:35 +0200

    - character ids adapted in ts and CSS (first id = 0 instead of id = 1)

--- src/app/shared/models/character-database.ts
+++ src/app/shared/models/character-database.ts
@@ -1,8 +1,8 @@
-import { Character } from './character';
+import {Character} from './character';
 
 export const CHARACTERS: Character[] = [
-  {id: 1, name: 'Jasmin', image: '../assets/Gui_Elemente/GUI_MainMenu/Avatar_1.png', assigned: false, ready: false},
-  {id: 2, name: 'Phteven', image: '../assets/Gui_Elemente/GUI_MainMenu/Avatar_2.png', assigned: false, ready: false},
-  {id: 3, name: 'McJagger', image: '../assets/Gui_Elemente/GUI_MainMenu/Avatar_3.png', assigned: false, ready: false},
-  {id: 4, name: 'Simon', image: '../assets/Gui_Elemente/GUI_MainMenu/Avatar_4.png', assigned: false, ready: false}
+  {id: 0, name: 'Jasmin', image: '../assets/Gui_Elemente/GUI_MainMenu/Avatar_1.png', assigned: false, ready: false},
+  {id: 1, name: 'Phteven', image: '../assets/Gui_Elemente/GUI_MainMenu/Avatar_2.png', assigned: false, ready: false},
+  {id: 2, name: 'McJagger', image: '../assets/Gui_Elemente/GUI_MainMenu/Avatar_3.png', assigned: false, ready: false},
+  {id: 3, name: 'Simon', image: '../assets/Gui_Elemente/GUI_MainMenu/Avatar_4.png', assigned: false, ready: false}
 ];
