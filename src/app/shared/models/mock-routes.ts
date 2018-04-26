4b1b0f16b8445aa17c776c2f3e215c354a989769
joelbarmettler.UZH <joel.barmettler@uzh.ch>
2018-04-26 12:27:00 +0200

    Added end-path go chosable paths #15

--- src/app/shared/models/mock-routes.ts
+++ src/app/shared/models/mock-routes.ts
@@ -4,8 +4,5 @@ export const ROUTES: Route[] = [
   {id: 0, name: 'First Quest', path: 0},
   {id: 0, name: 'Hills of Gold', path: 1},
   {id: 0, name: 'Home Stretch', path: 2},
-  {id: 99, name: 'Ending fast', path: 3}
-  // {id: 5, name: 'Route5'},
-  // {id: 6, name: 'Route6'},
-  // {id: 7, name: 'Route7'},
+  {id: 98, name: 'Ending fast', path: 3}
 ];
